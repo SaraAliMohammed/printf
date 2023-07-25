284f52b53a1ffb0ebda882a646551c351e6c5c78
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-07-25 13:33:27 -0700

    Edit _printf file

--- _printf.c
+++ _printf.c
@@ -1,53 +1,22 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdarg.h>
 
 /**
- * _printf - produces output according to a format.
- * @format: a character string.
+ * _printf -  produces output according to a format.
+ * @format: character string.
  *
- * Return: the number of characters printed (excluding the null byte used to
- *         end output to strings)
- */
-int _printf(const char *format, ...) {
-	va_list args;
-	va_start(args, format);
-	int count = 0;
+ * Return: the number of characters printed.
+*/
 
-	while (*format != '\0') {
-		if (*format == '%') {
-			format++;
-			switch (*format) {
-				case 'c': {
-					char c = va_arg(args, int);
-					putchar(c);
-					count++;
-					break;
-				}
-				case 's': {
-					char *s = va_arg(args, char *);
-					fputs(s, stdout);
-					count += strlen(s);
-					break;
-				}
-				case '%': {
-					putchar('%');
-					count++;
-					break;
-				}
-				default: {
-					putchar('%');
-					putchar(*format);
-					count += 2;
-					break;
-				}
-			}
-		} else {
-			putchar(*format);
-			count++;
-		}
-		format++;
-	}
+int _printf(const char *format, ...)
+{
+	int counter = 0;
+	va_list arguments_list;
 
-	va_end(args);
-	return count;
+	va_start(arguments_list, format);
+	if (!format || !format[counter])
+		return (-1);
+	counter = handle_delimiter(format, arguments_list);
+	va_end(arguments_list);
+	return (counter);
 }
